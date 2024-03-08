54fe97df84e8148cb56563a20d1e0508f9a37dd5
Eliagiac <elia.giaccardi@gmail.com>
2024-03-08 11:34:49 +0100

    Fetch data from database

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+import pandas as pd
 import psycopg2
 
 connection = psycopg2.connect(database="master", user="postgres", password="0000")
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM employees")
+employee_data = pd.DataFrame(cursor.fetchall(), columns=['id', 'name', 'surname'])
+connection.close()
+
+print(employee_data)
